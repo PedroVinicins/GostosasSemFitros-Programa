d99d51c363f683a5dbfeead32eb91f665afee237
Pedro Vinicius Vale7 <pedrn770@gmail.com>
2025-06-13 00:57:03 -0300

    FirebaseServer

---
+++ server.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Configuração do Firebase
+const apikey = process.env.API_KEY;
+const authDomain = process.env.AUTH_DOMAIN;
+const databaseURL = process.env.DATABASE_URL;
+const projectId = process.env.PROJECT_ID;
+const storageBucket = process.env.STORAGE_BUCKET;
+const messagingSenderId = process.env.MESSAGING_SENDER_ID;
+const appId = process.env.APP_ID;
+const measurementId = process.env.MEASUREMENT_ID;
+const firebaseConfig = {
+    apiKey: apikey,
+    authDomain: authDomain,
+    databaseURL: databaseURL,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+    measurementId: measurementId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
